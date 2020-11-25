5c9ccf71ee384c6c17cfcd3813c4b50591c89789
Manoj Panda <mkpanda13@gmail.com>
2020-11-25 16:22:20 -0500

    Add files via upload
    This is the BCS file that is used to download and unzip the files we receive everyday .

---
+++ 2_Download_Unzip_bcs.py
@@ -0,0 +1,24 @@
+#!/usr/local/bin/python3
+
+import pysftp
+from zipfile import ZipFile 
+
+with pysftp.Connection('10.88.15.177', username='ETL_MOH_Srv1', private_key='/home/infaadm/.ssh/id_rsa') as sftp:
+
+  # get paramiko SFTPClient object
+  psftp = sftp.sftp_client
+
+  # change remote directory
+  sftp.chdir('/BCS')
+
+  for attr in sftp.listdir():
+    if (attr[-4:] == '.zip'):
+      # download a remote file
+      sftp.get(attr)
+      # move it to processed folder
+      psftp.rename('/BCS/' + attr, '/BCS/processed/' + attr)
+      # unzip the file
+      with ZipFile(attr) as myzip:
+        for n in myzip.namelist():
+          print (n)
+        myzip.extractall()
